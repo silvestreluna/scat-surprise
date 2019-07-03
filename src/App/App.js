cec8589e82f5470e20ff611ba7ef698a6804ec03
Silvestre Luna <slvtrluna@gmail.com>
2019-07-02 19:27:09 -0500

    public and private routes working

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  Route,
+  Redirect,
+  BrowserRouter,
+  Switch,
+} from 'react-router-dom';
 import firebase from 'firebase/app';
 
 import MyNavBar from '../components/MyNavBar/MyNavBar';
@@ -8,6 +14,26 @@ import fbConnection from '../helpers/data/connection';
 import './App.scss';
 
 fbConnection();
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = props => (authed === false
+    ? (<Component {...props}/>)
+    : (
+      (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />)
+    )
+  );
+  return <Route {...rest} render={props => routeChecker(props)}/>;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = props => (authed === true
+    ? (<Component {...props}/>)
+    : (
+      (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />)
+    )
+  );
+  return <Route {...rest} render={props => routeChecker(props)}/>;
+};
 
 class App extends React.Component {
   state = {
@@ -30,19 +56,24 @@ class App extends React.Component {
 
   render() {
     const { authed } = this.state;
-    const loadComponent = () => {
-      if (authed) {
-        console.error(authed, 'home');
-        return <Home />;
-      }
-      console.error('teting');
-      return <Auth />;
-    };
+
 
     return (
       <div className="App">
-        <MyNavBar authed={authed} />
-        {loadComponent()}
+        <BrowserRouter>
+          <React.Fragment>
+            <MyNavBar authed={authed} />
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <PublicRoute path='/auth' component={Auth} authed={authed} />
+                  <PrivateRoute path='/home' component={Home} authed={authed} />
+                  <Redirect from="*" to="/auth" />
+                </Switch>
+              </div>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
