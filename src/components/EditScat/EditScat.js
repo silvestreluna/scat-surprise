eb01172731dd09d94d62e67992622050e0da5b50
Silvestre Luna <slvtrluna@gmail.com>
2019-07-02 20:34:36 -0500

    added single view and edits page, displaying id

--- src/components/EditScat/EditScat.js
+++ src/components/EditScat/EditScat.js
@@ -4,9 +4,11 @@ import './EditScat.scss';
 
 class EditScat extends React.Component {
   render() {
+    const editId = this.props.match.params.id;
     return (
       <div className="EditScat">
         <h1>EditScat</h1>
+        <h2>The EditId is: {editId}</h2>
       </div>
     );
   }
